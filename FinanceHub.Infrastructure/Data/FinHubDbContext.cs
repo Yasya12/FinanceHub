fe0af46a32e078d0c1063d6d5c5ea39f5aa1f82d
jasja <jasja200411@gmail.com>
2024-09-15 18:48:18 +0300

    feat: create DbContext

--- FinanceHub.Infrastructure/Data/FinHubDbContext.cs
+++ FinanceHub.Infrastructure/Data/FinHubDbContext.cs
@@ -1,6 +1,10 @@
+using FinanceHub.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace FinanceHub.Infrastructure.Data;
 
-public class FinHubDbContext
+public class FinHubDbContext : DbContext
 {
-    
+    public DbSet<User> Users { get; set; }
+    public FinHubDbContext(DbContextOptions<FinHubDbContext> options):base(options) {}
 }
