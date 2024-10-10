0591e9ae05892ef6d66f3c358673d0d33167e28a
jasja <jasja200411@gmail.com>
2024-10-10 13:26:39 +0300

    feat: add method GetByEmailAsync to IUserRepository and implement it in UserRepository

--- FinanceHub.Application/Interfaces/Repositories/IUserRepository.cs
+++ FinanceHub.Application/Interfaces/Repositories/IUserRepository.cs
@@ -2,4 +2,7 @@ using FinanceHub.Core.Entities;
 
 namespace FinanceGub.Application.Interfaces.Repositories;
 
-public interface IUserRepository : IGenericRepository<User> { }
+public interface IUserRepository : IGenericRepository<User>
+{
+    Task<User> GetByEmailAsync(string email);
+}
--- FinanceHub.Infrastructure/Repositories/UserRepository.cs
+++ FinanceHub.Infrastructure/Repositories/UserRepository.cs
@@ -1,10 +1,25 @@
 using FinanceGub.Application.Interfaces.Repositories;
 using FinanceHub.Core.Entities;
+using FinanceHub.Core.Exceptions;
 using FinanceHub.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinanceHub.Infrastructure.Repositories;
 
 public class UserRepository : GenericRepository<User>, IUserRepository
 {
-    public UserRepository(FinHubDbContext context) : base(context) { }
+    public UserRepository(FinHubDbContext context) : base(context){ }
+    
+    public async Task<User> GetByEmailAsync(string email)
+    {
+        try
+        {
+            return await _dbSet.FirstOrDefaultAsync(u => u.Email == email);
+        }
+        catch (RepositoryException ex)
+        {
+            throw new RepositoryException("An error occurred while trying to retrieve the user by email.", ex);
+        }
+    }
+
 }
