e133b2622df324ced76536d9e2a30224e6e2dce7
jasja <jasja200411@gmail.com>
2024-10-11 16:58:33 +0300

    test: Added unit tests for user commands
    - test: CreateUserCommandTests
      - test: CreateUser_SuccessfullyCreatesUser_WhenValidDataProvided
      - test: CreateUser_ThrowsException_WhenUserAlreadyExists
    
    - test: UpdateUserCommandTests
      - test: UpdateUser_SuccessfullyUpdatesUser_WhenValidDataProvided
      - test: UpdateUser_ThrowsNotFoundException_WhenUserDoesNotExist
    
    - test: DeleteUserCommandTests
      - test: DeleteUser_SuccessfullyDeletesUser_WhenUserExists
      - test: DeleteUser_ThrowsNotFoundException_WhenUserDoesNotExist

---
+++ FinanceHub.TestProject/Commands/User/CreateUserCommandTests.cs
@@ -0,0 +1,56 @@
+using FinanceGub.Application.Features.UserFeatures.Commands.CreateUserCommand;
+using FinanceGub.Application.Interfaces.Repositories;
+using FinanceHub.Core.Exceptions;
+using Moq;
+
+namespace FinanceHub.TestProject.Commands.User;
+
+public class CreateUserCommandTests
+{
+    private readonly Mock<IUserRepository> _mockUserRepository;
+    private readonly CreateUserCommandHandler _handler;
+
+    public CreateUserCommandTests()
+    {
+        _mockUserRepository = new Mock<IUserRepository>();
+        _handler = new CreateUserCommandHandler(_mockUserRepository.Object);
+    }
+    
+    private static readonly Core.Entities.User User = new()
+    {
+        Username = "user",
+        Email = "user@gmail.com",
+        PasswordHash = "validPasswordHash"
+    };
+
+    [Fact]
+    public async Task CreateUser_SuccessfullyCreatesUser_WhenValidDataProvided()
+    {
+        //Arrange
+        var command = new CreateUserCommand(User);
+
+        //Act
+        await _handler.Handle(command, default);
+
+        //Assert
+        _mockUserRepository.Verify(repo => repo.AddAsync(It.IsAny<Core.Entities.User>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task CreateUser_ThrowsException_WhenUserAlreadyExists()
+    {
+        //Arrange
+        var command = new CreateUserCommand(User);
+        
+        // Симулюємо, що користувач уже існує в базі даних.
+        _mockUserRepository
+            .Setup(repo => repo.GetByEmailAsync(It.IsAny<string>()))
+            .ReturnsAsync(User);
+        
+        //Act & Assert
+        var exception = await Assert.ThrowsAsync<ValidationException>(() => _handler.Handle(command, default));
+        
+        Assert.Equal($"User with email {User.Email} already exists.", exception.Message);
+        _mockUserRepository.Verify(repo => repo.AddAsync(It.IsAny<Core.Entities.User>()), Times.Never);
+    }
+}
---
+++ FinanceHub.TestProject/Commands/User/UpdateUserCommandTests.cs
@@ -0,0 +1,62 @@
+using FinanceGub.Application.Features.UserFeatures.Commands.UpdateUserCommand;
+using FinanceGub.Application.Interfaces.Repositories;
+using FinanceHub.Core.Exceptions;
+using Moq;
+
+namespace FinanceHub.TestProject.Commands.User;
+
+public class UpdateUserCommandTests
+{
+    private readonly Mock<IUserRepository> _mockRepository;
+    private readonly UpdateUserCommandHandler _handler;
+
+    public UpdateUserCommandTests()
+    {
+        _mockRepository = new Mock<IUserRepository>();
+        _handler = new UpdateUserCommandHandler(_mockRepository.Object);
+    }
+    
+    private static readonly Core.Entities.User User = new()
+    {
+        Id = Guid.NewGuid(),
+        Username = "user",
+        Email = "user@gmail.com",
+        PasswordHash = "validPasswordHash"
+    };
+    
+    [Fact]
+    public async Task UpdateUser_SuccessfullyUpdatesUser_WhenValidDataProvided()
+    {
+        //Arrange
+        var command = new UpdateUserCommand(User);
+        
+        _mockRepository
+            .Setup(repo => repo.GetByIdAsync(User.Id, It.IsAny<string>()))
+            .ReturnsAsync(User);
+
+        //Act
+        await _handler.Handle(command, default);
+
+        //Assert 
+        _mockRepository.Verify(repo => repo.UpdateAsync(It.Is<Core.Entities.User>(u => u.Id == User.Id)), Times.Once);
+    }
+
+    [Fact]
+    public async Task UpdateUser_ThrowsNotFoundException_WhenUserDoesNotExist()
+    {
+        //Arrange
+        var command = new UpdateUserCommand(User);
+
+        _mockRepository
+            .Setup(repo => repo.GetByIdAsync(User.Id, It.IsAny<string>()))
+            .ReturnsAsync((Core.Entities.User)null!);
+
+        //Act
+        var exception = await Assert.ThrowsAsync<NotFoundException>(() => _handler.Handle(command, default));
+
+        //Assert 
+        Assert.Equal($"User with ID {User.Id} not found.", exception.Message);
+        _mockRepository.Verify(repo => repo.UpdateAsync(User), Times.Never);
+    }
+
+}
