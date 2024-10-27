ba0bacf7e8d99c00af2f853453987c5df54b6f30
jasja <jasja200411@gmail.com>
2024-10-27 19:19:04 +0200

    feat: add mapping profile for UpdateUserDto to User and Profile entities

--- FinanceHub.Infrastructure/Mapping/UserProfileMappingProfile.cs
+++ FinanceHub.Infrastructure/Mapping/UserProfileMappingProfile.cs
@@ -13,12 +13,25 @@ public class UserProfileMappingProfile: Profile
             .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Username))
             .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
             .ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => PasswordHasher.HashPassword(src.Password)));
+        
+        CreateMap<UpdateUserDto, User>()
+            .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Username))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+            .ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => PasswordHasher.HashPassword(src.Password)))
+            .ForMember(dest => dest.Role, opt => opt.MapFrom(src => FinanceGub.Application.Identity.IdentityData.UserUserClaimName));
 
         CreateMap<CreateUserDto, FinanceHub.Core.Entities.Profile>()
             .ForMember(dest => dest.PhoneNumber, opt => opt.Ignore())
             .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.Country))
             .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth))
             .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
+            .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
+            .ForMember(dest => dest.UserId, opt => opt.Ignore());
+        
+        CreateMap<UpdateUserDto, FinanceHub.Core.Entities.Profile>()
+            .ForMember(dest => dest.PhoneNumber, opt => opt.Ignore())
+            .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.Country))
+            .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth))
             .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
             .ForMember(dest => dest.UserId, opt => opt.Ignore());
     }
