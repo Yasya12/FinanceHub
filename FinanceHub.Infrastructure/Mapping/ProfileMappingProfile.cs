f9009aa655b506aaac73a494bfd5e0a8e6283b25
jasja <jasja200411@gmail.com>
2024-10-28 18:43:22 +0200

    feat: write mapping for UpdateProfileDto and Profile.

--- FinanceHub.Infrastructure/Mapping/ProfileMappingProfile.cs
+++ FinanceHub.Infrastructure/Mapping/ProfileMappingProfile.cs
@@ -13,6 +13,11 @@ public class ProfileMappingProfile : Profile
             .ForMember(dest => dest.User, opt => opt.Ignore()) 
             .ReverseMap();
         
+        CreateMap<UpdateProfileDto, FinanceHub.Core.Entities.Profile>()
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore()) 
+            .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))  
+            .ReverseMap();
+        
         CreateMap<FinanceHub.Core.Entities.Profile, GetProfileDto>()
             .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.Username))
             .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email))
@@ -20,5 +25,13 @@ public class ProfileMappingProfile : Profile
             .ForMember(dest => dest.ProfilePictureUrl, opt => opt.MapFrom(src => src.ProfilePictureUrl))
             .ForMember(dest => dest.Bio, opt => opt.MapFrom(src => src.Bio))
             .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth));
+        
+        CreateMap<FinanceHub.Core.Entities.Profile, UpdateProfileDto>()
+            .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
+            .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.Country))
+            .ForMember(dest => dest.ProfilePictureUrl, opt => opt.MapFrom(src => src.ProfilePictureUrl))
+            .ForMember(dest => dest.Bio, opt => opt.MapFrom(src => src.Bio))
+            .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth));
+
     }
 }
