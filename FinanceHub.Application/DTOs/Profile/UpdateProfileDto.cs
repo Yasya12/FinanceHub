3dd4bda4d5910cf6198d5287fa05fc267002c60c
jasja <jasja200411@gmail.com>
2024-10-29 19:55:10 +0200

    update: made country and date of birth optional in UpdateProfileDto, changed profile picture type

--- FinanceHub.Application/DTOs/Profile/UpdateProfileDto.cs
+++ FinanceHub.Application/DTOs/Profile/UpdateProfileDto.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 
 namespace FinanceGub.Application.DTOs.Profile;
 
@@ -7,17 +8,14 @@ public class UpdateProfileDto
     [Phone(ErrorMessage = "Invalid phone number format.")]
     public string? PhoneNumber { get; set; }
 
-    [Required(ErrorMessage = "Country is required.")]
     [StringLength(50, ErrorMessage = "Country name cannot be longer than 50 characters.")]
-    public string Country { get; set; }
+    public string? Country { get; set; }
 
-    [Url(ErrorMessage = "Invalid URL format for profile picture.")]
-    public string? ProfilePictureUrl { get; set; }
+    public IFormFile? ProfilePictureUrl { get; set; }
 
     [StringLength(500, ErrorMessage = "Bio cannot be longer than 500 characters.")]
     public string? Bio { get; set; }
 
-    [Required(ErrorMessage = "Date of Birth is required.")]
     [DataType(DataType.Date, ErrorMessage = "Invalid date format.")]
-    public DateTime DateOfBirth { get; set; }
+    public DateTime? DateOfBirth { get; set; }
 }
