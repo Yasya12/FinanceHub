e4f1843241c6a50b0afa2af4e5c91d342b0cfd59
jasja <jasja200411@gmail.com>
2024-10-30 19:16:59 +0200

    feat: create Post entity

---
+++ FinanceHub.Domain/Entities/Post.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FinanceHub.Core.Entities;
+
+public class Post : Base
+{
+    [Required]
+    public Guid AuthorId { get; set; }
+
+    [StringLength(100, ErrorMessage = "Title cannot exceed 100 characters.")]
+    public string Title { get; set; }
+
+    [Required]
+    public string Content { get; set; }
+
+    [Required]
+    public DateTime CreatedAt { get; set; }
+
+    [Required]
+    public DateTime UpdatedAt { get; set; }
+
+    [Required]
+    public virtual User Author { get; set; }
+
+    //public virtual ICollection<Category> Categories { get; set; }
+    //public virtual ICollection<Comment> Comments { get; set; }
+
+}
