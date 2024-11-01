449773314e7892cc41a5455b14f7d0ad372ef234
jasja <jasja200411@gmail.com>
2024-11-01 15:22:54 +0200

    refactor: update PostMappingProfile for create and get post dtos

--- FinanceHub.Infrastructure/Mapping/PostMappingProfile.cs
+++ FinanceHub.Infrastructure/Mapping/PostMappingProfile.cs
@@ -10,13 +10,18 @@ public class PostMappingProfile : Profile
     {
         CreateMap<Post, GetPostDto>()
             .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Author.Username))
+            .ForMember(dest => dest.CategoryNames, opt => opt.MapFrom(src => src.PostCategory.Select(pc => pc.Category.Name)))
             .ReverseMap();
             
         CreateMap<CreatePostDto, Post>()
             .ForMember(dest => dest.AuthorId, opt => opt.Ignore())
             .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(_ => DateTime.UtcNow)) 
             .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(_ => DateTime.UtcNow)) 
-            .ForMember(dest => dest.Author, opt => opt.Ignore()); 
+            .ForMember(dest => dest.Author, opt => opt.Ignore())
+            .ForMember(dest => dest.PostCategory, opt => opt.MapFrom(src => src.CategoryNames.Select(name => new PostCategory
+            {
+                Category = new Category { Name = name }
+            })));
 
         CreateMap<UpdatePostDto, Post>()
             .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(_ => DateTime.UtcNow)) 
