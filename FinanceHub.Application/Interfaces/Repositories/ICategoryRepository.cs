c17db9ab403673341a3e4e9b10fdc851dd5374fc
jasja <jasja200411@gmail.com>
2024-11-01 15:22:19 +0200

    feat: add GetCategoriesByNamesAsync method  to get categories by name

--- FinanceHub.Application/Interfaces/Repositories/ICategoryRepository.cs
+++ FinanceHub.Application/Interfaces/Repositories/ICategoryRepository.cs
@@ -2,4 +2,7 @@ using FinanceHub.Core.Entities;
 
 namespace FinanceGub.Application.Interfaces.Repositories;
 
-public interface ICategoryRepository : IGenericRepository<Category> { }
+public interface ICategoryRepository : IGenericRepository<Category>
+{
+    Task<List<Category>> GetCategoriesByNamesAsync(IEnumerable<string> categoryNames);
+}
--- FinanceHub.Infrastructure/Repositories/CategoryRepository.cs
+++ FinanceHub.Infrastructure/Repositories/CategoryRepository.cs
@@ -1,10 +1,25 @@
 using FinanceGub.Application.Interfaces.Repositories;
 using FinanceHub.Core.Entities;
 using FinanceHub.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinanceHub.Infrastructure.Repositories;
 
 public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
 {
     public CategoryRepository(FinHubDbContext context) : base(context){ }
+    
+    public async Task<List<Category>> GetCategoriesByNamesAsync(IEnumerable<string> categoryNames)
+    {
+        if (categoryNames == null || !categoryNames.Any())
+        {
+            return new List<Category>(); 
+        }
+
+        var categories = await _dbSet
+            .Where(c => categoryNames.Contains(c.Name)) 
+            .ToListAsync();
+
+        return categories;
+    }
 }
