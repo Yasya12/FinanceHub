ca614cfa424851122c1aa002d9a4f60272015c2e
jasja <jasja200411@gmail.com>
2024-11-02 20:56:47 +0200

    feat: update UpdatePostDto to include a list of categories

--- FinanceHub.Application/DTOs/Post/UpdatePostDto.cs
+++ FinanceHub.Application/DTOs/Post/UpdatePostDto.cs
@@ -6,6 +6,7 @@ public class UpdatePostDto
 {
     [StringLength(100, ErrorMessage = "Title cannot exceed 100 characters.")]
     public string Title { get; set; }
+    public IEnumerable<string>? CategoryNames { get; set; }
 
     [Required]
     public string Content { get; set; }
