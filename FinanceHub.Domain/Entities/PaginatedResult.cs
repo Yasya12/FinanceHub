21cc2d496c7520dbc646dcb7296c9d6107439bdc
jasja <jasja200411@gmail.com>
2024-11-03 16:59:45 +0200

    feat: created PaginatedResult class for pagination implementation

---
+++ FinanceHub.Domain/Entities/PaginatedResult.cs
@@ -0,0 +1,9 @@
+namespace FinanceHub.Core.Entities;
+
+public class PaginatedResult<T>
+{
+    public IEnumerable<T> Items { get; set; }
+    public int TotalCount { get; set; }
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+}
