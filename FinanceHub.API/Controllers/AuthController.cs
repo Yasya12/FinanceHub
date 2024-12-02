06a79482f0360c10b64aa8873f71a5409723d3f1
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2024-12-03 01:15:29 +0200

    feat: add Google login functionality to AuthController with token validation and user creation

--- FinanceHub.API/Controllers/AuthController.cs
+++ FinanceHub.API/Controllers/AuthController.cs
@@ -1,9 +1,12 @@
 using FinanceGub.Application.DTOs.Authentication;
 using FinanceGub.Application.Interfaces.Serviсes;
+using Google.Apis.Auth;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinanceHub.Controllers;
 
+[ApiController]
+[Route("api/[controller]")]
 public class AuthController : ControllerBase
 {
     private readonly IJwtService _jwtService;
@@ -11,7 +14,7 @@ public class AuthController : ControllerBase
     private readonly ILogger<AuthController> _logger;
 
     public AuthController(IJwtService jwtService, IUserService userService, ILogger<AuthController> logger)
-    {
+    {   
         _jwtService = jwtService;
         _userService = userService;
         _logger = logger;
@@ -30,5 +33,35 @@ public class AuthController : ControllerBase
         }
 
         return Unauthorized();
+    }
+    
+    [HttpPost("google")]
+    public async Task<IActionResult> GoogleLogin([FromBody] TokenRequest tokenRequest)
+    {
+        _logger.LogInformation("Received Google login request");
+        try
+        {
+            _logger.LogInformation("Validating Google token");
+            // Перевіряємо токен через Google API
+            var payload = await GoogleJsonWebSignature.ValidateAsync(tokenRequest.Token);
+
+            // Шукаємо користувача за Google ID (Subject)
+            var user = await _userService.GetUserByGoogleIdAsync(payload.Subject);
+            
+            // Якщо користувач не знайдений, створюємо нового
+            if (user == null)
+            {
+                user = await _userService.CreateUserFromGoogleAsync(payload);
+            }
+
+            // Генеруємо токен для користувача
+            var token = _jwtService.GenerateToken(user);
+            return Ok(new { token });
+        }
+        catch (InvalidJwtException)
+        {
+            _logger.LogInformation("Validating Google token");
+            return Unauthorized("Invalid Google Token");
+        }
     }
 }
