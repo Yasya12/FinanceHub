4203bfa9b1ce19a8bec1275c4e2c661045fc3e46
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2024-12-18 19:18:21 +0200

    config: register new services in DI

--- FinanceHub.Infrastructure/DependencyInjection.cs
+++ FinanceHub.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,10 @@
 using System.Reflection;
+using FinanceGub.Application.Interfaces;
 using FinanceGub.Application.Interfaces.Repositories;
 using FinanceGub.Application.Interfaces.Serviсes;
 using FinanceHub.Infrastructure.Repositories;
 using FinanceHub.Infrastructure.Services;
+using FinanceHub.Infrastructure.SignalR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -29,6 +31,7 @@ public static class DependencyInjection
         serviceCollection.AddScoped<ICategoryRepository, CategoryRepository>();
         serviceCollection.AddScoped<IPostCategoryRepository, PostCategoryRepository>();
         serviceCollection.AddScoped<ICommentRepository, CommentRepository>();
+        serviceCollection.AddScoped<ILikeRepository, LikeRepository>();
         
         serviceCollection.AddScoped<IUserService, UserService>(); 
         serviceCollection.AddScoped<IProfileService, ProfileService>(); 
@@ -36,6 +39,9 @@ public static class DependencyInjection
         serviceCollection.AddScoped<IPostService, PostService>();
         serviceCollection.AddScoped<ICategoryService, CategoryService>();
         serviceCollection.AddScoped<ICommentService, CommentService>();
+        serviceCollection.AddScoped<ILikeService, LikeService>();
+        serviceCollection.AddScoped<IAuthService, AuthService>();
+        serviceCollection.AddScoped<ILikeHub, LikeHub>();
         
         serviceCollection.AddAutoMapper(Assembly.GetExecutingAssembly());
     }
