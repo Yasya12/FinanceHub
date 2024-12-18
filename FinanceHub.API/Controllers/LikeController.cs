dd2f1f9795fd1bc8920907e97ab717270a2e09ec
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2024-12-18 19:19:41 +0200

    feat: create like controller to update like count

---
+++ FinanceHub.API/Controllers/LikeController.cs
@@ -0,0 +1,36 @@
+using System.Security.Claims;
+using FinanceGub.Application.Interfaces.Serviсes;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceHub.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LikeController : ControllerBase
+{
+    private readonly ILikeService _likeService;
+
+    public LikeController(ILikeService likeService)
+    {
+        _likeService = likeService;
+    }
+
+    [HttpPost("toggle-like/{postId}")]
+    [Authorize]
+    public async Task<IActionResult> ToggleLike(Guid postId)
+    {
+        var token = Request.Headers["Authorization"].FirstOrDefault();
+
+        // Отримуємо userId з JWT токена
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (!Guid.TryParse(userId, out var parsedUserId))
+        {
+            return Unauthorized("User is not authenticated");
+        }
+
+        var likeAdded = await _likeService.ToggleLikeAsync(postId, parsedUserId);
+        return Ok(new { success = likeAdded });
+    }
+}
