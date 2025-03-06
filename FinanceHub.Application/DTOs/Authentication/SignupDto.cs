0c8363db9a748fc76e9926999b24c14210cf5ce2
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-03-06 21:04:05 +0200

    create Signup dto

---
+++ FinanceHub.Application/DTOs/Authentication/SignupDto.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FinanceGub.Application.DTOs.Authentication;
+
+public class SignupDto
+{
+    [Required]
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    public string Email { get; set; }
+
+    [Required]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
+    [MaxLength(100, ErrorMessage = "Password must be at most 100 characters long.")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$",
+        ErrorMessage =
+            "Password must have at least one uppercase letter, one lowercase letter, one number, and one special character.")]
+    public string Password { get; set; }
+
+    [Required]
+    [StringLength(20, MinimumLength = 3)]
+    [RegularExpression(@"^[a-zA-Z0-9_]+$",
+        ErrorMessage = "Username can only contain letters, numbers, and underscores.")]
+    public string Username { get; set; }
+}
